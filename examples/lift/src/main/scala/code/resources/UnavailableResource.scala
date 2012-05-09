9da5e36625c8f3c0f1086f74f17e3e6209fd70db
Jordan West <jordanrw@gmail.com>
2012-05-08 19:20:10 -0700

    get lift example project compiling again

--- examples/lift/src/main/scala/code/resources/UnavailableResource.scala
+++ examples/lift/src/main/scala/code/resources/UnavailableResource.scala
@@ -1,8 +1,8 @@
 package code.resources
 
-import com.github.jrwest.scalamachine.core.{SimpleResult, ReqRespData, Resource}
+import com.github.jrwest.scalamachine.core.{ValueRes, ReqRespData, Resource}
 
 
 class UnavailableResource extends Resource {
-  override def serviceAvailable(data: ReqRespData) = SimpleResult(false,data)
+  override def serviceAvailable(data: ReqRespData) = (ValueRes(false),data)
 }
